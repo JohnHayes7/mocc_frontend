8f785b6dce872f673854c31c892eb18dfab9d12b
John Hayes <JohnnyHayes7@gmail.com>
2020-05-18 16:59:07 -0400

    first dive in, building landing page header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return(
+        <div id="mocc-head">
+            <div className="head-category">Premiers</div>
+            <div className="head-category" >QA</div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const LandingPage = () => {
+    return(
+        <div>
+            <Header />
+            <h1>Welcome to the MOCC</h1>
+        </div>
+    )
+}
+
+export default LandingPage
