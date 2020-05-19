a11640657f0974e366b1a1a666867454d474d505
John Hayes <JohnnyHayes7@gmail.com>
2020-05-19 10:54:23 -0400

    added some styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './header.css'
 
+
 const Header = () => {
     return(
-        <div id="mocc-head">
-            <div className="head-category">Premiers</div>
-            <div className="head-category" >QA</div>
+        <div className="head-foot">
+            <div className="categories">
+                <span id="company">MC Media Ops Control Center</span>
+                <span className="category">Premiers</span>
+                <span className="category">QA Projects</span>
+            </div>
         </div>
     )
 }
