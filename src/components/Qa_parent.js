9adcbe1fa17a30d80b8670192915252bbbcccf97
John Hayes <JohnnyHayes7@gmail.com>
2020-05-19 23:09:57 -0400

    building out test qa project

--- src/components/Qa_parent.js
+++ src/components/Qa_parent.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from './Header'
+import './qa_parent.css'
 
 class QaParent extends React.Component{
 
     render(){
         return(
-            <div>QA!!!!!!</div>
+            <div>
+                <Header />
+                <h1>QA HOME</h1>
+                <div id="projects">
+                    <div className="project-box">
+                        <span className="proj-title">Test1</span>
+                    </div>
+                    <div className="project-box">
+                        <span className="proj-title">Test2</span>
+                    </div>
+                    <div className="project-box">
+                        <span className="proj-title">Test3</span>
+                    </div>
+                    <div className="project-box">
+                        <span className="proj-title">Test4</span>
+                    </div>
+                </div>
+           </div>
         )
     }
 
