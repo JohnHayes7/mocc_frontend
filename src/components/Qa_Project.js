489b315301304faa77d11b6f09139b7b093248d4
John Hayes <JohnnyHayes7@gmail.com>
2020-05-21 09:16:03 -0400

    created .env file and added .env to gitignore

--- src/components/Qa_Project.js
+++ src/components/Qa_Project.js
@@ -3,7 +3,17 @@ import './qa_project.css'
 class QaProject extends React.Component{
     render(){
         return(
-            <div className='project-div'><h1>QA Project: Test 1</h1></div>
+            <div className='project-div'>
+                <div className="project-header">
+                    <h1>QA Project: Test 1</h1>
+                    <div id="login-opts">
+                        <span><strong>Login as Reviewer</strong></span>
+                        <br></br>
+                        <br></br>
+                        <span><strong>Login as Admin</strong></span>
+                    </div>
+                </div>
+            </div>
         )
     }
 
