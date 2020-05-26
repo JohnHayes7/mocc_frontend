61db2439b39885f6877f9e0972f50333ec5a0827
John Hayes <JohnnyHayes7@gmail.com>
2020-05-26 15:59:35 -0400

    debugging selection tracking process

--- src/components/Qa_Project_Info.js
+++ src/components/Qa_Project_Info.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './qa_project.css'
-import AssetTicket from './assetTicket'
+import AssetTicket from './AssetTicket'
 
 
 
@@ -8,16 +8,31 @@ import AssetTicket from './assetTicket'
 const QaProjectInfo = props =>{
     console.log(props.details)
 
+    const selectedVodIds = []
     
-    const parseAssetDetails = () => {
-       return props.details.videos.map(video => <AssetTicket key={video.mvVodId} video={video}/>)       
+    const addVideoToSelected = (vodId) =>{
+        selectedVodIds.push(vodId)
+        console.log(selectedVodIds)
     }
+
+    const removeVideoFromSelected = (vodId) =>{
+        console.log(vodId)
+    }
+    
+
+    const parseAssetDetails = () => {
+        return props.details.videos.map(video => <AssetTicket key={video.mvVodId} video={video} addVideoToSelected={addVideoToSelected} removeVideoFromSelected={removeVideoFromSelected}/>)       
+     }
+
+
+
+
 
 
     return(
-        <form id="asset-info">
+        <div id="asset-info">
             {parseAssetDetails()}
-        </form>
+        </div>
     )
 }
 
--- src/components/assetTicket.js
+++ src/components/assetTicket.js
@@ -2,25 +2,30 @@ import React, { useState } from 'react'
 
 const AssetTicket = props =>{
 
-const [selected, toggleSelected] = useState(false);
+    const [selected, toggleSelected] = useState(false);
 
-    const select = e => {
-        e.preventDefault()
-        debugger
-        // console.log(e.target)
+    const select = event => {
+        event.preventDefault()
+        const vodId = event.target.dataset.id
         toggleSelected(!selected)
-
+        props.addVideoToSelected(vodId)
+        
     }
 
-    const unselect = e =>{
-        e.preventDefault()
+    const unselect = event =>{
+        event.preventDefault()
+        const vodId = event.target.dataset.id
         toggleSelected(!selected)
+        props.removeVideoFromSelected(vodId)
     }
+    
+
+   
 
      if(selected){
-        return <div className="selected-asset-display" data-id={props.video.mvVodId} onClick={e => unselect(e)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>    
+        return <div className="selected-asset-display" data-id={props.video.mvVodId} onClick={event => unselect(event)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>    
      }else{
-        return  <div className="asset-display" data-id={props.video.mvVodId} onClick={e => select(e)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>
+        return  <div className="asset-display" data-id={props.video.mvVodId} onClick={event => select(event)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>
      }
 }
 
