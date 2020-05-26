4f81a54bbb6f90636d8b8d58a84d29b22fd3a575
John Hayes <JohnnyHayes7@gmail.com>
2020-05-26 13:51:37 -0400

    added styling to assets

--- src/components/Qa_Project_Info.js
+++ src/components/Qa_Project_Info.js
@@ -5,17 +5,17 @@ import './qa_project.css'
 const QaProjectInfo = props =>{
     console.log(props.details)
 
-    const parseProjectDetails = () => {
+    const parseAssetDetails = () => {
        return props.details.videos.map(video => {
            console.log(video)
-        return<div className="project-display" key={video.mvVodId}><h3>VODID: {video.mvVodId}</h3><h3>{video.title}</h3><h4>{video.artist}</h4></div>
+        return<div className="asset-display" key={video.mvVodId}><h3>VODID: {video.mvVodId}</h3><h3>{video.title}</h3><h4>{video.artist}</h4></div>
        })
        
     }
 
     return(
-        <div>
-            {parseProjectDetails()}
+        <div id="asset-info">
+            {parseAssetDetails()}
         </div>
     )
 }
