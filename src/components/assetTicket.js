32703dcace1d54c4a3fbe17d47359a0daf8a32bc
John Hayes <JohnnyHayes7@gmail.com>
2020-05-26 14:19:11 -0400

    can select certain assets

--- src/components/assetTicket.js
+++ src/components/assetTicket.js
@@ -4,9 +4,11 @@ const AssetTicket = props =>{
 
 const [selected, setSelected] = useState(false);
 
- return(
-    <div className="asset-display" key={props.video.mvVodId} onClick={() => setSelected(!selected)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>
- )   
+     if(selected){
+        return <div className="selected-asset-display" key={props.video.mvVodId} onClick={() => setSelected(!selected)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>    
+     }else{
+        return  <div className="asset-display" key={props.video.mvVodId} onClick={() => setSelected(!selected)}><h3>VODID: {props.video.mvVodId}</h3><h3>{props.video.title}</h3><h4>{props.video.artist}</h4></div>
+     }
 }
 
 export default AssetTicket
