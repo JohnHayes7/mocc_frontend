1efe16522f713fdba95111252b94f55d3081c684
John Hayes <JohnnyHayes7@gmail.com>
2020-05-26 22:44:28 -0400

    started added selection tracking capabilities

--- src/components/Qa_Project_Info.js
+++ src/components/Qa_Project_Info.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './qa_project.css'
 import AssetTicket from './AssetTicket'
 
@@ -9,11 +9,14 @@ const QaProjectInfo = props =>{
     console.log(props.details)
 
     const selectedVodIds = []
+
+    const [count, setCount] = useState(0)
     
     const addVideoToSelected = (vodId) =>{
         if(!selectedVodIds.includes(vodId)){
             selectedVodIds.push(vodId)
             console.log(selectedVodIds)
+            setCount(count + 1)
         }
     }
 
@@ -21,12 +24,23 @@ const QaProjectInfo = props =>{
         const index = selectedVodIds.indexOf(vodId)
         selectedVodIds.splice(index, 1)
         console.log(selectedVodIds)
+        setCount(count - 1)
     }
     
 
     const parseAssetDetails = () => {
         return props.details.videos.map(video => <AssetTicket key={video.mvVodId} video={video} addVideoToSelected={addVideoToSelected} removeVideoFromSelected={removeVideoFromSelected}/>)       
      }
+
+    const selectionInfo = () => {
+        return(
+            <div className="selection-div">
+                <h4>Selected Videos: {count} </h4>
+            </div>
+        )
+    }
+
+    
 
 
 
@@ -38,8 +52,8 @@ const QaProjectInfo = props =>{
             <div className="details-column">
                 {parseAssetDetails()}
             </div>
-            <div className="next-column">
-                More Info
+            <div className="selection-column">
+                {count > 0 ? selectionInfo() : <h4>Select Videos for Details</h4>}
             </div>
         </div>
     )
